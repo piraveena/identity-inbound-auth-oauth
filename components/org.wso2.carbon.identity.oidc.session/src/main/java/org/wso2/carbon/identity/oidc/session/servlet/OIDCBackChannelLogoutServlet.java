6421e7d40c2e953f0099890fd3e934bc9c115968
Piraveena <piraveena18@gmail.com>
2017-09-04 22:21:41 +0530

    back-channel logout servlet added

---
+++ components/org.wso2.carbon.identity.oidc.session/src/main/java/org/wso2/carbon/identity/oidc/session/servlet/OIDCBackChannelLogoutServlet.java
@@ -0,0 +1,165 @@
+package org.wso2.carbon.identity.oidc.session.servlet;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.JSONObject;
+import org.wso2.carbon.identity.oidc.session.OIDCSessionConstants;
+import org.wso2.carbon.identity.oidc.session.OIDCSessionState;
+import org.wso2.carbon.identity.oidc.session.util.OIDCSessionManagementUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Created by piraveena on 9/4/17.
+ */
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.JSONObject;
+import org.wso2.carbon.identity.oidc.session.OIDCSessionConstants;
+import org.wso2.carbon.identity.oidc.session.OIDCSessionState;
+import org.wso2.carbon.identity.oidc.session.util.OIDCSessionManagementUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Set;
+import java.util.UUID;
+
+import com.hazelcast.web.SessionState;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.JSONObject;
+import org.wso2.carbon.identity.oidc.session.OIDCSessionConstants;
+import org.wso2.carbon.identity.oidc.session.OIDCSessionState;
+import org.wso2.carbon.identity.oidc.session.util.OIDCSessionManagementUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Created by piraveena on 9/1/17.
+ */
+public class OIDCBackChannelLogoutServlet extends HttpServlet {
+    private static final Log log= LogFactory.getLog(OIDCBackChannelLogoutServlet.class);
+
+    public void init() throws ServletException {
+        log.info("OIDC Backchannel logout servlet has been started");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        Cookie obpscookie= OIDCSessionManagementUtil.getOPBrowserStateCookie(request);
+
+        String obpsCookieValue=obpscookie.getValue();
+
+
+
+        log.info("OIDCBackChannelLogoutServlet: accessing get method ");
+        log.info(request.toString());
+        PrintWriter print=response.getWriter();
+        String sid=getSidClaim(getSessionState(obpsCookieValue));
+        print.write(sid);
+        removeSession(obpscookie,response);
+
+    }
+
+    /**
+     * returns the session state of the obps cookie
+     *
+     * @param sessionId
+     * @return sessionState of the obps cookie
+     */
+
+    public OIDCSessionState getSessionState(String sessionId) {
+
+        OIDCSessionState sessionState = OIDCSessionManagementUtil.getSessionManager()
+                .getOIDCSessionState(sessionId);
+        return sessionState;
+    }
+
+    /**
+     * return client id of all the RPs belong to same session
+     *
+     * @param sessionState
+     * @return client id of all the RPs belong to same session
+     */
+
+    public Set<String> getSessionParticipants(OIDCSessionState sessionState) {
+
+        Set<String> sessionParticipants = sessionState.getSessionParticipants();
+        return sessionParticipants;
+    }
+
+    /**
+     * returns the sid of the all the RPs belong to same session
+     *
+     * @param sessionState
+     * @return sid of the all the RPs belong to same session
+     */
+    public String getSidClaim(OIDCSessionState sessionState) {
+
+        String sidClaim = sessionState.getSidClaim();
+        return sidClaim;
+    }
+
+    public void buildLogoutToken(OIDCSessionState sessionState){
+        for(String clientId: getSessionParticipants(sessionState)) {
+            String sub = sessionState.getAuthenticatedUser();
+            String aud=clientId;
+            String value= UUID.randomUUID().toString();
+            JSONObject event=new JSONObject();
+            event.put( "http://schemas.openid.net/event/backchannel-logout", new JSONObject());
+            String iss = "https://localhost:9443/carbon/";
+
+//            long lifetimeInMillis = Integer.parseInt(config.getOpenIDConnectIDTokenExpiration()) * 1000;
+            long curTimeInMillis = Calendar.getInstance().getTimeInMillis();
+            Date iat=new Date(curTimeInMillis);
+
+
+        }
+
+    }
+
+    public Cookie removeSession(Cookie cookie, HttpServletResponse response){
+        if (cookie.getName().equals(OIDCSessionConstants.OPBS_COOKIE_ID)) {
+            cookie.setMaxAge(0);
+            cookie.setSecure(true);
+            cookie.setPath("/");
+            response.addCookie(cookie);
+            return cookie;
+        }
+        return null;
+    }
+
+}
+
+
+
